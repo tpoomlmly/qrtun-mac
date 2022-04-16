bd3c874b1c5a79409698651be6b29bdecea73867
Tomii Poll <tlp1g19@soton.ac.uk>
2022-04-16 22:05:40 +0200

    Add documentation and move into function

--- utun.py
+++ utun.py
@@ -6,25 +6,40 @@ import socket
 from fcntl import ioctl
 import struct
 
-CTLIOCGINFO = 3227799043
-STRUCT_FORMAT = "<I96s"
+CTLIOCGINFO = 3227799043  # _IOWR('N', 3, struct ctl_info)
+CTL_INFO_FORMAT = "<I96s"  # little-endian uint32_t, then a char[96]
 
 
-# Get file descriptor for socket
-utun_socket = socket.socket(socket.PF_SYSTEM, socket.SOCK_DGRAM, socket.SYSPROTO_CONTROL)
-file_descriptor = utun_socket.fileno()
+def get_utun() -> socket.socket:
+    """
+    Create a utun device and get a socket connection to its controller.
 
-# Make struct ctl_info
-ctl_info = struct.pack(STRUCT_FORMAT, 0, "com.apple.net.utun_control".encode())
-# Exchange controller name for ID + name
-ctl_info = ioctl(utun_socket, CTLIOCGINFO, ctl_info)
-controller_id, controller_name = struct.unpack(STRUCT_FORMAT, ctl_info)
+    Anything sent via the newly created utun device is received on this socket.
+    Anything sent from this socket is received by those listening on the utun device.
+    This function will crash if not run on macOS, since PF_SYSTEM and SYSPROTO_CONTROL
+    only exist on macOS.
 
-# Connect + activate (requires sudo)
-utun_socket.connect((controller_id, 0))  # 0 means pick the next convenient one
+    :return: a socket.socket() instance corresponding to a newly created utun adapter
+    """
+    # Get file descriptor for socket
+    utun_socket = socket.socket(socket.PF_SYSTEM, socket.SOCK_DGRAM, socket.SYSPROTO_CONTROL)
 
-while True:
-    # infinite loop to keep the interface open
-    message = utun_socket.recv(1500)
-    print(message)
-    utun_socket.send(message)
+    # Make struct ctl_info
+    ctl_info = struct.pack(CTL_INFO_FORMAT, 0, "com.apple.net.utun_control".encode())
+    # Exchange controller name for ID + name
+    ctl_info = ioctl(utun_socket, CTLIOCGINFO, ctl_info)
+    controller_id, controller_name = struct.unpack(CTL_INFO_FORMAT, ctl_info)
+
+    # Connect + activate (requires sudo)
+    utun_socket.connect((controller_id, 0))  # 0 means pick the next convenient number
+
+    return utun_socket
+
+
+if __name__ == "__main__":
+    utun = get_utun()
+    while True:
+        # infinite loop to keep the interface open
+        message = utun.recv(1500)
+        print(message)
+        utun.send(message)
