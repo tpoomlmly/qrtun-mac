de97dbbadaa5c738cf766b6378b917b7d187cc38
Tomii Poll <tlp1g19@soton.ac.uk>
2022-04-25 20:03:37 +0100

    Move display update to main thread

--- main.py
+++ main.py
@@ -10,23 +10,19 @@ def main():
     utun = Utun()
     print(f"Interface name: {utun.name}")
     qr_display = QRDisplay()
-    qr_reader = QRReader()
 
     transmit_thread = threading.Thread(target=transmit_loop, args=(utun, qr_display))
-    receive_thread = threading.Thread(target=receive_loop, args=(utun, qr_reader))
-
+    receive_thread = threading.Thread(target=receive_loop, args=(utun,))
     transmit_thread.start()
     receive_thread.start()
-    transmit_thread.join()
-    # while True:
-    #     message = utun.recv(utun.mtu)
-    #     print(message)
-    #     utun.send(message)
+
+    while True:
+        qr_display.update_window()
 
 
 def transmit_loop(utun: Utun, qr_display: QRDisplay) -> None:
     """
-    Read data being sent into the utun device and display it as a QR code.
+    Read data being sent into the utun device and set it to be displayed as a QR code.
 
     :param utun: the virtual network device to read from
     :param qr_display: the display to write the QR codes on
@@ -35,15 +31,14 @@ def transmit_loop(utun: Utun, qr_display: QRDisplay) -> None:
         message = utun.recv(utun.mtu)
         print(message)
         utun.send(message)
-        qr_display.show(message)
+        qr_display.set_data(message)
 
 
-def receive_loop(utun: Utun, qr_reader: QRReader) -> None:
+def receive_loop(utun: Utun) -> None:
     """
     Read QR codes and make the data in them appear on the utun device.
 
     :param utun: the virtual network device receiving the QR data
-    :param qr_reader: the device reading QR codes
     """
 
 
